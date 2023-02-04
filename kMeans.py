56783156e6e7b523839035d7d50316b21f82e074
AvihaiDidi <avihai.didi@gmail.com>
2023-02-04 14:50:34 +0200

    removed FAKmeans code, and restructed the rest of the code to be around comparing the CPU and CUDA implementaions instead

--- kMeans.py
+++ kMeans.py
@@ -1,7 +1,7 @@
 # CUDA accelerated k-means for image processing using Nvidia's numba library
 
 import sysAndConsts as sac
-import FAkMeansCUDA
+import kMeansCPU
 import kMeansCUDA
 
 from os.path import isfile, join
@@ -9,7 +9,7 @@ from os import listdir
 import imghdr
 
 # process a single image located at 'path', returns -1 if couldn't and 0 otherwise
-def singleImg(path, iterations, k, printLog = False, dest = "", fast = False):
+def singleImg(path, iterations, k, printLog = False, dest = "", CPU = False):
     if printLog:
         print(f"Attempting to load {path}")
     try:
@@ -20,14 +20,14 @@ def singleImg(path, iterations, k, printLog = False, dest = "", fast = False):
         return -1
     if printLog:
         print(f"{path} loaded, making centroids now")
-    if fast:
-        centroids = FKkMeansCUDA.getCentroids(iterations, k, img_data[0], printprog = False)
+    if CPU:
+        centroids = kMeansCPU.getCentroids(iterations, k, img_data[0], printprog = False)
     else:
         centroids = kMeansCUDA.getCentroids(iterations, k, img_data[0], printprog = False)
     if printLog:
         print("Done getting centroids, making new image")
-    if fast:
-        new_img = FKkMeansCUDA.newImg(img_data, centroids)
+    if CPU:
+        new_img = kMeansCPU.newImg(img_data, centroids)
     else:
         new_img = kMeansCUDA.newImg(img_data, centroids)
     if printLog:
@@ -43,19 +43,19 @@ def singleImg(path, iterations, k, printLog = False, dest = "", fast = False):
     return 0
 
 # process a list of images given in 'pathlist', returns the number of images that were processed succsusfully
-def multiImg(pathlist, iterations, k, printLog = False, dest = "", fast = False):
+def multiImg(pathlist, iterations, k, printLog = False, dest = "", CPU = False):
     count = 0
     for path in pathlist:
-        ret = singleImg(path, iterations, k, printLog, dest, fast)
+        ret = singleImg(path, iterations, k, printLog, dest, CPU)
         if ret == 0:
             count += 1
     return count
 
 # get a path to a folder and process all images in that folder, save the result in {dest}
-def allInFolder(path, iterations, k, printLog = False, dest = "", fast = False):
+def allInFolder(path, iterations, k, printLog = False, dest = "", CPU = False):
     pathlist = [f"{path}/{f}" for f in listdir(path) if isfile(join(path, f)) and f.lower().endswith(sac.IMAGE_FORMATS)]
     if printLog:
         print(f"The folder {path} contains {len(pathlist)} images.")
-    count = multiImg(pathlist, iterations, k, printLog = False, dest = f"{dest}", fast = False)
+    count = multiImg(pathlist, iterations, k, printLog = False, dest = f"{dest}", CPU = False)
     if printLog:
         print(f"Done processing the folder {path} for k = {k}\n{count}/{len(pathlist)} images were processed succsusfully.")
